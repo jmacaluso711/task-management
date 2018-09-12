25f3c93ba10f32dbc5f46dee32fcbbec600d8620
John Macaluso <John.Macaluso@infor.com>
2018-09-12 12:04:21 -0400

    added checkbox function

--- src/App.js
+++ src/App.js
@@ -1,46 +1,53 @@
 import React, { Component } from 'react';
 
-const Task = ({ task }) => (
+const Task = ({ task, completeTask }) => (
   <li>
+    <input type="checkbox" onChange={completeTask} />
     <h2>{task.name}</h2>
     <p>{task.description}</p>
     <span>{task.dueDate}</span>
+    <button>x</button>
   </li>
 )
 
 class App extends Component {
   state = {
-    tasks: JSON.parse(localStorage.getItem('tasks')) || [],
+    tasks: [],
     done: false
   }
 
-  createTask = (e) => {
+  addTask = (e) => {
     e.preventDefault();
-    let task = {
+    const task = {
       name: this.name.value,
       description: this.description.value,
       dueDate: this.dueDate.value
     }
-    localStorage.setItem('tasks', JSON.stringify(this.state.tasks));
     this.setState({ 
       tasks: [...this.state.tasks, task]
     });
     this.taskForm.reset();
   }
 
+  completeTask = (e, key) => {
+    console.log(key);
+  }
+
   render() {
     const { tasks } = this.state;
+
     return (
       <div>
         <ul>
-          {tasks.map((task, i) => <Task key={i} task={task} />) }
+          {tasks.map((task, i) => <Task key={i} task={task} completeTask={(e, key) => this.completeTask(e, key)} />)}
         </ul>
-        <form ref={(el) => this.taskForm = el} onSubmit={(e) => this.createTask(e)}>
+        <form ref={(el) => this.taskForm = el} onSubmit={(e) => this.addTask(e)}>
           <div>
             <input 
               ref={(input) => this.name = input}
               name="name"
               type="text"
+              required
             />
           </div>
           <div>
@@ -49,6 +56,7 @@ class App extends Component {
               name="description" 
               cols="30" 
               rows="5"
+              required
             ></textarea>
           </div>
           <div>
@@ -56,9 +64,10 @@ class App extends Component {
               ref={(input) => this.dueDate = input}
               name="dueDate"
               type="date"
+              required
             />
           </div>
-          <button>Submit</button>
+          <button>+ Add Task</button>
         </form>
       </div>
     );
