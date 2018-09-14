a805ae964d0db79f65043fb6e993e70921f16007
John Macaluso <macaluso.john@gmail.com>
2018-09-13 21:56:58 -0400

    remove toggle function from task items

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,10 +3,6 @@ import styled from 'styled-components';
 import moment from 'moment';
 
 export default class Task extends Component {
-  state = {
-    viewDetail: false
-  }
-
   /**
    * Pass the task index to our App component
    * so we can toggle the complete value
